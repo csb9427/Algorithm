3904fcec347851d2e898832014d7318731894fc7
csb9427 <80771842+csb9427@users.noreply.github.com>
2021-09-07 13:09:15 +0900

    Create 5522_카드 게임.c

---
+++ 구현/5522_카드 게임.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, c, d, e;
+	scanf("%d", &a);
+	scanf("%d\n", &b);
+	scanf("%d\n", &c);
+	scanf("%d\n", &d);
+	scanf("%d", &e);
+	printf("%d", a+b+c+d+e);
+	return 0;
+}
