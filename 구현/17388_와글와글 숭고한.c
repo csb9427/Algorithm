c64241b5e5a5bc4cc9464dacb081983764902874
csb9427 <80771842+csb9427@users.noreply.github.com>
2021-10-07 00:24:40 +0900

    Create 17388_와글와글 숭고한.c

---
+++ 구현/17388_와글와글 숭고한.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if(a+b+c>=100){
+		printf("OK");
+	}
+	else if(a+b+c<100){
+		if(a<b && a<c){
+			printf("Soongsil");
+		}
+		else if(b<a && b<c){
+			printf("Korea");
+		}
+		else{
+			printf("Hanyang");
+		}
+	}
+	return 0;
+}
