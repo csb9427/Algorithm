2d0f1cbb745795acc58dcb4f31ab1ff674ffb59c
csb9427 <80771842+csb9427@users.noreply.github.com>
2021-10-10 03:27:23 +0900

    Create 10768_특별한 날.c

---
+++ 구현/10768_특별한 날.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	scanf("%d\n %d", &a, &b);
+	if(a<2){
+		printf("Before");
+	}
+	else if(a>=3){
+		printf("After");
+	}
+	else if(a==2){
+		if(b>18){
+			printf("After");
+		}
+		else if(b<18){
+			printf("Before");
+		}
+		else if(b==18){
+			printf("Special");
+		}
+	}
+	return 0;
+}
