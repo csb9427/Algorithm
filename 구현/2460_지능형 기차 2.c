3130bcea3efe59179aeaf3eb6c28b4af54eb1249
csb9427 <csb9427@naver.com>
2021-12-27 23:40:21 +0900

    Create 2460_지능형 기차 2.c

---
+++ 구현/2460_지능형 기차 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, c=-9999999, i, d=0;
+	for(i=0; i<10; i++){
+		scanf("%d %d", &a, &b);
+		d=d-a;
+		d=d+b;
+		if(c<d){
+			c=d;
+		}
+	}
+	printf("%d", c);
+	return 0;
+}
