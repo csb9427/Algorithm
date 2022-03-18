3db315354601df7ae9c2c698ee67a37d97e921c3
csb9427 <csb9427@naver.com>
2022-03-18 16:39:08 +0900

    Create 10992_별 찍기-17.cc

---
+++ 구현/10992_별 찍기-17.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		for (int j = 2 * a - 1; j > i + a - 1; j--) {
+			cout << " ";
+		}
+		for (int q = 0; q < i * 2 - 1; q++) {
+			if (i == a) {
+				cout << "*";
+			}
+			else {
+				if (q == 0 || q == i * 2 - 2) {
+					cout << "*";
+				}
+				else {
+					cout << " ";
+				}
+			}
+		}
+		cout << '\n';
+	}
+	return 0;
+}
